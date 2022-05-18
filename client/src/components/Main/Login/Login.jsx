36d42acf2ae1ae55bb83591189cc5ffb0ea525cf
elbolaestevez <elbolaestevez@gmail.com>
2022-05-18 09:01:43 -0300

    donee

--- client/src/components/Main/Login/Login.jsx
+++ client/src/components/Main/Login/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
     
     
     try {
-  const res = await axios.post(`http://localhost:5000/api/astronomy/user/login`,data);
+  const res = await axios.post(`/api/astronomy/user/login`,data);
    if (res.status === 200) {
   
     console.log(res);
